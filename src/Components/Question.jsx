95e5368fa95e450d516cb3ed3be86458229c8b3a
alenm777 <alenm.1717@gmail.com>
2023-09-29 11:30:10 -0300

    preguntas?

---
+++ src/Components/Question.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Question = () => {
+  return (
+    <>
+    <div className='flex flex-col justify-between shadow-md shadow-slate-300 w-[600px] h-[600px] p-10 rounded-lg'>
+        <div className='flex justify-between'>
+            <span className='text-xl font-bold'>
+
+            </span>
+
+<div>
+<span className='font-semibold'>
+Dificultad: 
+</span>
+<span className='font-bold'>
+
+</span>
+</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Question
--- src/Pages/Categorias.jsx
+++ src/Pages/Categorias.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { questions } from '../Data'
+import Question from '../Components/Question';
+
+const shuffleArray = (array) => {
+  const newArray = array.sort(() => Math.random() - 0.5);
+  return newArray.slice(0, 5);
+};
 
 const Categorias = () => {
- 
   const { category } = useParams()
     console.log(category)
+
+    const [questionsFiltered, setQuestionsFiltered] = useState(
+      questions.filter(question => question.category === category)
+      );
+
+      useEffect(() => {
+const newQuestions = shuffleArray(questionsFiltered);
+setQuestionsFiltered(newQuestions);
+      }, []);
   
   return (
-    <div>Categorias</div>
+    <div className='container flex flex-col items-center justify-center gap-10'
+     style={{height: 'calc(100vh - 5rem)' }}
+    >
+   <Question />
+    </div>
   )
 }
 
