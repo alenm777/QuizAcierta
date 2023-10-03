32b143d417869b6c422f3f6391f15f71288bbcd5
alenm777 <alenm.1717@gmail.com>
2023-10-03 18:55:47 -0300

    perro

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -7,17 +7,38 @@ indexQuestion,
 setIndexQuestion,
 setActiveQuiz,
 }) => {
-
-    const [answerRandom, setAnswerRandom] = useState([]);
-
+const [score, setScore] = useState(0)
+const [selectAnswerIndex, setSelectAnswerIndex] = useState(null);
+const [answered, setAnswerd] = useState(false);
+const [answerRandom, setAnswerRandom] = useState([]);
+const [activeResults, setActiveResults] = useState(false);
 
     useEffect(() => {
-const answer = []
-    }, [filteredQuestion])
+const answer = [
+    ...filteredQuestion.incorrect_answers,
+    filteredQuestion.correct_answer,
+];
+
+setAnswerRandom(answer.sort(() => Math.random() - 0.5));
+    }, [filteredQuestion]);
+
+    const checkAnswer = (answerText, index) => {
+        if(answerText === filteredQuestion.correct_answer) {
+            setScore(score + 1 );
+        }
+    setSelectAnswerIndex(index)
+   setAnswerd(true)
+    };
 
     const onNextQuestion = () => {
         setIndexQuestion(indexQuestion + 1 )
-    } 
+    }; 
+
+    const onReset = () => {
+    setScore(0);
+    setActiveQuiz(false);
+    setIndexQuestion(0);
+    };
 
   return (
     <>
@@ -40,7 +61,7 @@ Dificultad: </span>
 </div>
 <button
  className='border px-5 py-2 rounded-lg font-bold transition-all hover:bg-yellow-500 hover:text-gray-900'
- onClick={() => setActiveQuiz(false)}
+ onClick={ onReset }
 >
 Reiniciar
 </button>
@@ -52,18 +73,27 @@ Reiniciar
 
 {/* Respuestas */}
 <div className='grid grid-cols-2 gap-5'>
- { /*  Respuestas correctas e incorrectas */}
-<button className='border -p-5 rounded-lg flex justify-center items-center hover: scale-105'>
+
+  { /*  Respuestas correctas e incorrectas */}
+ { answerRandom.map((answer, index) => (
+<button
+ className='border -p-5 rounded-lg flex justify-center items-center hover: scale-105'
+key={answer}
+onClick={() => checkAnswer(answer, index)}
+>
 <p className='font-medium text-center text-sm'> 
-Alguna Respueta 
+{answer}
 </p>
 </button>
+ ))}
 </div>
+
 {/* boton de siguiente pregunta o finalizar*/}
 <button 
 className='border-2 border-yellow-600 text-yellow-600 rounded-md px-5 py-2
 hover:bg-yellow-600 hover:text-black font-medium'
- onClick={onNextQuestion} >
+ onClick={onNextQuestion}
+>
 Finalizar o siguiente pregunta
 </button>
     </div>
